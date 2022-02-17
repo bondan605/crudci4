1b764ed6200a5bbc86997083dbd7014653760580
Bondan Gunawan <bondangunawan77@gmail.com>
2022-02-17 11:14:53 +0700

    fitur login

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
     /**
      * @var string
      */
-    public $fromEmail;
+    public $fromEmail = 'soundw49@gmail.com';
 
     /**
      * @var string
      */
-    public $fromName;
+    public $fromName = 'BondanOfficial';
 
     /**
      * @var string
